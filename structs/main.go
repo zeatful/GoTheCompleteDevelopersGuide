c587ed73b28f6b2269fc4a305e27e6eaf3b3b718
zeatful <zeatful@users.noreply.github.com>
2018-06-09 21:40:57 -0400

    getting started with structs

---
+++ structs/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	john := person{firstName: "John", lastName: "Doe"}
+	fmt.Println(john)
+}
