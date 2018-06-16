fdda8c82474dfab885c135996d0bcfb5b5dd44e1
zeatful <zeatful@users.noreply.github.com>
2018-06-16 13:57:07 -0400

    check link function implementation

--- channels/main.go
+++ channels/main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	urls := []string{"google.com", "facebook.com", "stackoverflow.com", "golang.org", "amazon.com"}
-	fmt.Print(urls)
+	urls := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, url := range urls {
+		checkLink(url)
+	}
+}
+
+func checkLink(url string) {
+	_, err := http.Get(url) // blocking call
+	if err != nil {
+		fmt.Println(url, "might be down!")
+	} else {
+		fmt.Println(url, "is up!")
+	}
 }
