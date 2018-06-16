f3cb27a9c57ac6d9587f14205940939235321475
zeatful <zeatful@users.noreply.github.com>
2018-06-16 14:09:39 -0400

    go routine start and notes

--- channels/main.go
+++ channels/main.go
@@ -15,7 +15,23 @@ func main() {
 	}
 
 	for _, url := range urls {
-		checkLink(url)
+		/*
+			By default GO attempts to only use one core:
+			go keyword will allow a different go routine to run
+			when one blocks.  So still only one routine runs at a
+			time when on a single core
+
+			When GO is setup to use multiple cores
+			each GO routine will get a core and can run
+			in parallel
+
+			CONCURRENCY IS NOT PARALLELISM
+			(hyperthreading) vs (multiple cores)
+
+			Main Routine exists after launching go routines
+			and then exits without showing any output
+		*/
+		go checkLink(url)
 	}
 }
 
@@ -23,7 +39,6 @@ func checkLink(url string) {
 	_, err := http.Get(url) // blocking call
 	if err != nil {
 		fmt.Println(url, "might be down!")
-	} else {
-		fmt.Println(url, "is up!")
 	}
+	fmt.Println(url, "is up!")
 }
